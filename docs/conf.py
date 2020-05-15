ea63604032eac055e97fce0d92a9918e561d4b81
Daniel Wertheimer (ZA) <Daniel.Wertheimer@absa.africa>
2020-05-15 15:55:47 +0200

    updated docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Daniel Wertheimer'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
